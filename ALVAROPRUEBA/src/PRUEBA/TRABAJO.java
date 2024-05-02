4f3e5fa484c0ca0633cabff534635988faf0c187
AlvaroPoblacionGarcia <alvaro.pobgar@educa.jcyl.es>
2024-05-02 11:10:59 +0200

    sasgf

--- ALVAROPRUEBA/src/PRUEBA/TRABAJO.java
+++
@@ -1,7 +0,0 @@
-package PRUEBA;
-
-public class TRABAJO {
-public static void main(String[] args) {
-	System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-}
-}
--- ALVAROPRUEBA/src/PRUEBA/trabajo2.java
+++
@@ -1,8 +0,0 @@
-package PRUEBA;
-
-public class trabajo2 {
-public static void main(String[] args) {
-	System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	System.out.println("asdasdsadsad");
-}
-}
