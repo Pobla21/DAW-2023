d019fccbeb9a0bbfad30a88f3631a1883e1d49b8
AlvaroPoblacionGarcia <alvaro.pobgar@educa.jcyl.es>
2024-05-02 11:53:12 +0200

    Cambio

--- PruebaGit/src/Prueba/Prueba.java
+++ PruebaGit/src/Prueba/Prueba.java
@@ -3,7 +3,10 @@ package Prueba;
 public class Prueba {
 public static void main(String[] args) {
 	System.out.println("Hola");
+<<<<<<< master
 	System.out.println("hola2");
 	System.out.println("hola3");
+=======
+>>>>>>> 976d214 Probamos2
 }
 }
