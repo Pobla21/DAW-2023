c631a32b3b3bb1ac65ce90f2239b22a774250a52
AlvaroPoblacionGarcia <alvaro.pobgar@educa.jcyl.es>
2024-05-02 10:59:06 +0200

    guardado

---
+++ ALVAROPRUEBA/src/PRUEBA/trabajo2.java
@@ -0,0 +1,8 @@
+package PRUEBA;
+
+public class trabajo2 {
+public static void main(String[] args) {
+	System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	System.out.println("asdasdsadsad");
+}
+}
